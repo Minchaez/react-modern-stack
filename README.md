5be86a016fab73b7072a22131242675141209650
Minchaez <rlawlsdl0730@gmail.com>
2026-04-12 16:34:56 +0900

    Initial Commit: tailwind css 사용을 위한 환경 세팅

---
+++ README.md
@@ -0,0 +1,1 @@
+# Zustand, Tanstack Query, TailwindCSS 학습을 위한 레포지토리
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="text-5xl font-bold underline">hello world</div>
+    </>
+  );
+}
+
+export default App;
