0765db019686832e85b272dec92ac787e7e9c2b4
Minchaez <rlawlsdl0730@gmail.com>
2026-04-18 15:55:48 +0900

    shadcn/ui & zustand 활용 TodoList 구현

--- src/components/todo-list/todo-item.tsx
+++ src/components/todo-list/todo-item.tsx
@@ -1,14 +1,19 @@
-export default function TodoItem({
-  id,
-  content,
-}: {
-  id: number;
-  content: string;
-}) {
+import type { Todo } from "@/types/todo";
+import { Button } from "../ui/button";
+import { useDeleteTodo } from "@/store/todos";
+
+export default function TodoItem({ id, content }: Todo) {
+  const deleteTodo = useDeleteTodo();
+  const handleDeleteClick = () => {
+    deleteTodo(id);
+  };
+
   return (
-    <div>
+    <div className="flex items-center justify-between border p-2">
       {content}
-      <button>삭제</button>
+      <Button variant={"destructive"} onClick={handleDeleteClick}>
+        삭제
+      </Button>
     </div>
   );
 }
